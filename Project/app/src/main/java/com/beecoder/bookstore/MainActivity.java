b554d76f32f30094d2d3d6ca1bc09947508556a1
sakib20 <sakib20@iut-dhaka.edu>
2020-10-05 17:16:36 +0600

    add whole project

---
+++ Project/app/src/main/java/com/beecoder/bookstore/MainActivity.java
@@ -0,0 +1,31 @@
+package com.beecoder.bookstore;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.beecoder.bookstore.Authentication.AuthActivity;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class MainActivity extends AppCompatActivity {
+    private FirebaseUser user;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        user = FirebaseAuth.getInstance().getCurrentUser();
+
+        if (user == null) {
+            openAuthActivity();
+            finish();
+        }
+    }
+
+    private void openAuthActivity() {
+        startActivity(new Intent(this, AuthActivity.class));
+    }
+}
