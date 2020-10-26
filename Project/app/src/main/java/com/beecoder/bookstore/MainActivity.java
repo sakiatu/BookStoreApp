bd4ae9bb2179f58d7f771af83e9ae1c44c7635e4
sakib20 <sakib20@iut-dhaka.edu>
2020-10-26 10:59:33 +0600

    add menu on toolbar in MainActivity

--- Project/app/src/main/java/com/beecoder/bookstore/MainActivity.java
+++ Project/app/src/main/java/com/beecoder/bookstore/MainActivity.java
@@ -2,8 +2,11 @@ package com.beecoder.bookstore;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import com.beecoder.bookstore.Authentication.AuthActivity;
 import com.google.firebase.auth.FirebaseAuth;
@@ -16,12 +19,34 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setupToolbar();
         user = FirebaseAuth.getInstance().getCurrentUser();
 
         if (user == null) {
             openAuthActivity();
             finish();
         }
+    }
+
+    private void setupToolbar() {
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        TextView title = toolbar.findViewById(R.id.tv_title_toolbar);
+        title.setText("BookStore");
+        toolbar.inflateMenu(R.menu.main_menu);
+        toolbar.setOnMenuItemClickListener(this::onMenuItemSelected);
+    }
+
+    private boolean onMenuItemSelected(MenuItem menuItem) {
+        switch (menuItem.getItemId()) {
+            case R.id.action_sell:
+                openSellingActivity();
+                break;
+        }
+        return true;
+    }
+
+    private void openSellingActivity() {
+
     }
 
     private void openAuthActivity() {
